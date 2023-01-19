740f3d3c2cc662e3bd9c0e544286064a99c00048
Timothy <timothy.mureithi@student.moringaschool.com>
2023-01-20 00:38:52 +0300

    star reating added for reviews form

---
+++ src/components/StarRating/StarRating2.js
@@ -0,0 +1,45 @@
+import React, { Component, useEffect, useState } from "react";
+import FilledStar from '../../assets/images/filled-star.svg';
+import EmptyStar from '../../assets/images/empty_star.svg';
+
+const StarRating = ({setAccuracy, accuracy}) => {
+
+    const [currRating, setCurrRating] = useState(5);
+    const [selectRating, setSelectRating] = useState(null);
+
+    const finalRating = selectRating || currRating
+
+    const onHover = (e) => {
+        if (e.target.className === "star") {
+            setCurrRating(e.target.dataset.value);
+        }
+      }
+    
+      const onClick= (e) => {
+        if (e.target.dataset.value === currRating) {
+            setSelectRating(e.target.dataset.value);
+        }
+      }
+
+      useEffect(() => {
+          setAccuracy(finalRating)
+      },[selectRating, currRating])
+
+    return [...Array(5).keys()].map((index) => {
+        return (
+        <img
+            key={index}
+            data-value={index + 1}
+            className="star"
+            onMouseOver={onHover}
+            onClick={onClick}
+            src={selectRating ? (index + 1 <= selectRating ? FilledStar : EmptyStar) : (index + 1 <= currRating ? FilledStar : EmptyStar)}
+        />
+        );
+    });
+
+}
+ 
+export default StarRating;
+
+
